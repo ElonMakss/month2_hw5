075e827b689bdde8717861197edb9ce60e686d7f
maks.kudrin.22@mail.ru <maks.kudrin.22@mail.ru>
2021-05-21 02:05:30 +0600

    final

---
+++ app/src/main/java/com/example/firstapp/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.firstapp;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView textView;
+    private Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.search);
+        button = findViewById(R.id.go);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://www.google.kg/search?q=" + textView.getText().toString()));
+                startActivity(intent);
+            }
+        });
+    }
+
+}
